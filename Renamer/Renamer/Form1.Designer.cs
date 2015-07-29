13d78f883a71ba31ec690f125020f2c88cf5b4f3
Matt Sonnhalter <matthew9559@hotmail.com>
2015-07-29 01:22:46 -0600

    Initial commit of code to selecting files to be renamed, entering a renamed scheme as well as confirmation of each file being renamed

--- Renamer/Renamer/Form1.Designer.cs
+++ Renamer/Renamer/Form1.Designer.cs
@@ -28,20 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.openFilesButton = new System.Windows.Forms.Button();
+            this.selectFilesLabel = new System.Windows.Forms.Label();
+            this.renameFilesButton = new System.Windows.Forms.Button();
+            this.renameSchemeTextBox = new System.Windows.Forms.TextBox();
+            this.renamingSchemeLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // openFilesButton
+            // 
+            this.openFilesButton.Location = new System.Drawing.Point(178, 32);
+            this.openFilesButton.Name = "openFilesButton";
+            this.openFilesButton.Size = new System.Drawing.Size(116, 28);
+            this.openFilesButton.TabIndex = 0;
+            this.openFilesButton.Text = "Open";
+            this.openFilesButton.UseVisualStyleBackColor = true;
+            this.openFilesButton.Click += new System.EventHandler(this.OpenFilesButton_Click);
+            // 
+            // selectFilesLabel
+            // 
+            this.selectFilesLabel.AutoSize = true;
+            this.selectFilesLabel.Location = new System.Drawing.Point(25, 32);
+            this.selectFilesLabel.Name = "selectFilesLabel";
+            this.selectFilesLabel.Size = new System.Drawing.Size(64, 13);
+            this.selectFilesLabel.TabIndex = 1;
+            this.selectFilesLabel.Text = "Select Files:";
+            // 
+            // renameFilesButton
+            // 
+            this.renameFilesButton.Location = new System.Drawing.Point(104, 164);
+            this.renameFilesButton.Name = "renameFilesButton";
+            this.renameFilesButton.Size = new System.Drawing.Size(136, 56);
+            this.renameFilesButton.TabIndex = 2;
+            this.renameFilesButton.Text = "Rename";
+            this.renameFilesButton.UseVisualStyleBackColor = true;
+            this.renameFilesButton.Click += new System.EventHandler(this.renameFilesButton_Click);
+            // 
+            // renameSchemeTextBox
+            // 
+            this.renameSchemeTextBox.Location = new System.Drawing.Point(141, 82);
+            this.renameSchemeTextBox.Name = "renameSchemeTextBox";
+            this.renameSchemeTextBox.Size = new System.Drawing.Size(203, 20);
+            this.renameSchemeTextBox.TabIndex = 3;
+            // 
+            // renamingSchemeLabel
+            // 
+            this.renamingSchemeLabel.AutoSize = true;
+            this.renamingSchemeLabel.Location = new System.Drawing.Point(25, 85);
+            this.renamingSchemeLabel.Name = "renamingSchemeLabel";
+            this.renamingSchemeLabel.Size = new System.Drawing.Size(97, 13);
+            this.renamingSchemeLabel.TabIndex = 4;
+            this.renamingSchemeLabel.Text = "Renaming Scheme";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(295, 270);
+            this.ClientSize = new System.Drawing.Size(375, 244);
+            this.Controls.Add(this.renamingSchemeLabel);
+            this.Controls.Add(this.renameSchemeTextBox);
+            this.Controls.Add(this.renameFilesButton);
+            this.Controls.Add(this.selectFilesLabel);
+            this.Controls.Add(this.openFilesButton);
             this.Name = "Form1";
             this.Text = "Media Renamer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button openFilesButton;
+        private System.Windows.Forms.Label selectFilesLabel;
+        private System.Windows.Forms.Button renameFilesButton;
+        private System.Windows.Forms.TextBox renameSchemeTextBox;
+        private System.Windows.Forms.Label renamingSchemeLabel;
     }
 }
 
--- Renamer/Renamer/Form1.cs
+++ Renamer/Renamer/Form1.cs
@@ -1,12 +1,83 @@
 ﻿using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
 
 namespace Renamer
 {
     public partial class Form1 : Form
     {
+        public List<string> TargetFiles = new List<string>();
+        public string TargetDirectory;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void OpenFilesButton_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog getFiles = new OpenFileDialog();
+            getFiles.Multiselect = true;
+
+            if (getFiles.ShowDialog() == DialogResult.OK)
+            {
+                TargetDirectory = getFiles.FileName;
+                foreach (string file in getFiles.SafeFileNames)
+                {
+                    TargetFiles.Add(file);
+                }
+            }
+        }
+
+        private void renameFilesButton_Click(object sender, System.EventArgs e)
+        {
+             List<string> filesBeforeTrim = new List<string>();
+            filesBeforeTrim = TargetFiles;
+
+            string directoryPath = Path.GetDirectoryName(TargetDirectory);
+            string fileExtension = Path.GetExtension(filesBeforeTrim[0]); //assumes all files have same extension
+            string renamingSchemeTrimmed = renameSchemeTextBox.Text.Remove(renameSchemeTextBox.Text.Length - 2);
+            List<string> newFilesName = new List<string>();
+            int episodes = 1;
+
+            //rename each file to be moved
+            foreach(string file in filesBeforeTrim)
+            {
+                string episodesPadded = episodes.ToString().PadLeft(2, '0');
+                newFilesName.Add(directoryPath + "\\" + renamingSchemeTrimmed + episodesPadded + fileExtension);
+                episodes++;               
+            }
+
+            //add directory to original files
+            //foreach (string ogFile in filesBeforeTrim)
+            //{
+            //    ogFilesWithDirectory.Add(directoryPath + '\\' + ogFile);
+            //}
+            //LINQ Version:
+            List<string> ogFilesWithDirectory = filesBeforeTrim.Select(ogFile => directoryPath + '\\' + ogFile).ToList();
+
+            using (var e1 = ogFilesWithDirectory.GetEnumerator())
+            using (var e2 = newFilesName.GetEnumerator())
+            try
+            {
+              while(e1.MoveNext() && e2.MoveNext())
+              {
+                if (e1.Current == e2.Current)
+                {
+                    MessageBox.Show("Files are already named as designated scheme");
+                    Application.Restart();                    
+                }
+
+                System.IO.File.Move(e1.Current,e2.Current);
+                MessageBox.Show("Files renamed succesfully");
+              }
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show("There was an issue with renmaing the file due to the following exception: " + ex.ToString());
+            }
         }
     }
 }
